bd6ec05e5316c73c41210459431dd5b3ebf513d7
Ricardo Miranda <ricardo.miranda@geopagos.com>
2023-01-26 15:35:07 -0300

    Bars visualizer

---
+++ bars.js
@@ -0,0 +1,113 @@
+const canvasSketch = require('canvas-sketch');
+const { mapRange } = require('canvas-sketch-util/math');
+const colormap = require('colormap');
+const cvWidth = 1080;
+const cvHeight = 1080;
+const fftSize = 64;
+const volume = 0.1;
+const barMinHeight = 0;
+const barMaxHeight = cvWidth * 0.4;
+const barWidth =  2 * cvWidth / fftSize;
+
+const barsColor = colormap({
+  colormap: 'jet',
+  nshades: fftSize * 0.5,
+  format: 'hex',
+  alpha: 1,
+})
+
+/** @type { HTMLAudioElement } */
+let audio;
+
+/** @type { AudioContext } */
+let audioContext;
+
+/** @type { Uint8Array } */
+let audioData;
+
+/** @type { MediaElementAudioSourceNode } */
+let sourceNode;
+
+/** @type { AnalyserNode } */
+let analyserNode;
+
+const settings = {
+  dimensions: [ cvWidth, cvHeight ],
+  animate: true,
+};
+
+function addListeners () {
+  window.addEventListener('click', () => {
+    if (!audioContext) {
+      createAudio();
+    }
+
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.pause();
+    }
+  });
+}
+
+function createAudio() {
+  audio = document.createElement('audio');
+  audio.src = 'audio/wasteland.mp3';
+  audio.volume = volume;
+  
+  audioContext = new AudioContext();
+  sourceNode = audioContext.createMediaElementSource(audio);
+  sourceNode.connect(audioContext.destination);
+
+  analyserNode = audioContext.createAnalyser();
+  sourceNode.connect(analyserNode);
+
+  analyserNode.fftSize = fftSize;
+  audioData = new Uint8Array(analyserNode.frequencyBinCount);
+}
+
+/**
+ * 
+ * @param { Uint8Array } audioData
+ * @param { CanvasRenderingContext2D } context 
+ */
+function visualize(audioData, context) {
+  let barHeight, x, y, color;
+  const topHeight = 20;
+
+  context.save();
+
+  context.lineWidth = 1;
+  for (let i = 0; i < audioData.length; i++) {
+    barHeight = mapRange(audioData[i], 0, 255, barMinHeight, barMaxHeight);
+    x = i * barWidth;
+    y = cvHeight - barHeight;
+    color = barsColor[i];
+    context.fillStyle = color;
+    context.strokeStyle = color;
+    context.fillRect(x, y, barWidth, barHeight);
+
+    context.fillStyle = 'white';
+    context.fillRect(x, y - barWidth - 0.5 * barWidth, barWidth, barWidth);
+  }
+
+  context.restore();
+}
+
+const sketch = () => {
+  addListeners();
+  return ({ context, width, height, frame }) => {
+
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, width, height);
+
+    if (!audioContext) {
+      return;
+    }
+
+    analyserNode.getByteFrequencyData(audioData);
+    visualize(audioData, context);
+  };
+};
+
+canvasSketch(sketch, settings);
