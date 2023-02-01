ff7c5b8a67023f6fa12e66779a679e63d1a89656
Ricardo Miranda <ricardo.miranda@geopagos.com>
2023-02-01 18:26:13 -0300

    Reestructure particles

--- particles/index.js
+++ particles/index.js
@@ -1,19 +1,13 @@
 const canvasSketch = require('canvas-sketch');
-const { rangeFloor } = require('canvas-sketch-util/random');
-const { Vector } = require('./vector');
+const { Particle } = require('./particle');
 const colormap = require('colormap');
 const { quadIn } = require('eases');
 
 const cvWidth = 1080;
 const cvHeight = 1080;
-
-const k = 0.5;
-const c = 0.8;
-const repelRadius = 40000;
 const circleCount = 15;
 const dotRadius = 10;
-const dotPadding = 4;
-const velocityDamp = 0.98;
+const dotPadding = 5;
 const colors = colormap({
   colormap: 'winter',
   nshades: circleCount,
@@ -28,90 +22,20 @@ const settings = {
   animate: true,
 };
 
-class Particle {
-  constructor({x, y, radius, color}) {
-    /** @type { Vector } */
-    this.pos = new Vector(x, y);
-    /** @type { Vector } */
-    this.origin = this.pos.copy();
-    /** @type { Vector } */
-    this.acc = new Vector(0, 0);
-    /** @type { Vector } */
-    this.vel = new Vector(0, 0);
-    /** @type { number } */
-    this.radius = radius;
-    /** @type { string } */
-    this.color = color;
-  }
-
-  /**
-   * 
-   * @param { Vector } force 
-   */
-  applyForce(force) {
-    this.acc.add(force);
-  }
-
-  attract() {
-    const force = this.origin.copy();
-    force.sub(this.pos);
-    if (force.mag === 0) {
-      return;
-    }
-    force.mult(k / force.mag);
-    this.applyForce(force);
-  }
-
-  repel(x, y) {
-    const dx = this.pos.x - x;
-    const dy = this.pos.y - y;
-    if (dx * dx + dy * dy > repelRadius) {
-      return;
-    }
-    const force = new Vector(dx, dy);
-    force.mult(c / force.mag);
-    this.applyForce(force);
-  }
-
-  update() {
-    this.vel.add(this.acc);
-    this.vel.mult(velocityDamp);
-    this.pos.add(this.vel);
-    this.acc.mult(0);
-  }
-
-  /**
-   * 
-   * @param { CanvasRenderingContext2D } context 
-   */
-  draw(context) {
-    console.log(this)
-    context.beginPath();
-    context.arc(this.pos.x, this.pos.y, this.radius, 0, 2 * Math.PI);
-    context.fillStyle = this.color;
-    context.strokeStyle = this.color;
-    context.stroke();
-    context.fill();
-  }
-}
-
 /**
  * 
  * @param { CanvasRenderingContext2D } context
  */
 function visualize(context) {
-  if (mouseX !== undefined && mouseY !== undefined) {
-    context.beginPath();
-    context.arc(mouseX, mouseY, Math.sqrt(repelRadius), 0, Math.PI * 2);
-    context.strokeStyle = '#666';
-    context.stroke();
-  }
   for (const p of particles) {
     if (mouseX !== undefined && mouseY !== undefined) {
       p.repel(mouseX, mouseY);
     }
     p.attract();
     p.update();
+  }
+  particles.sort((a, b) => a.scale - b.scale);
+  for (const p of particles) {
     p.draw(context);
   }
 }
@@ -130,17 +54,17 @@ function addListeners(canvas) {
 /** @type { Particle[] } */
 const particles = [];
 
-const sketch = ({canvas, width, height }) => {
+function createParticles(width, height) {
   let particleCount, phi, angleSize;
   const mx = width * 0.5;
   const my = height * 0.5;
-  const initAngleOffset = Math.PI / ( 2 * circleCount );
+  const initAngleOffset = Math.PI / (2 * circleCount);
   for (let i = 0; i < circleCount; i++) {
     if (i === 0) {
       particles.push(new Particle({
         x: mx,
         y: my,
-        radius: (1- quadIn(i / circleCount)) * dotRadius,
+        radius: (1 - quadIn(i / circleCount)) * dotRadius,
         color: colors[i]
       }));
     } else {
@@ -158,7 +82,10 @@ const sketch = ({canvas, width, height }) => {
       }
     }
   }
+}
 
+const sketch = ({canvas, width, height }) => {
+  createParticles(width, height);
   addListeners(canvas);
   return ({ context, width, height }) => {
     context.fillStyle = 'black';
---
+++ particles/particle.js
@@ -0,0 +1,79 @@
+import { mapRange } from 'canvas-sketch-util/math';
+const { Vector } = require('./vector');
+
+const d = 0.98;
+const k = 0.5;
+const c = 0.8;
+const repelRadius = 22500;
+const ratioRadius = 250000;
+
+export class Particle {
+  constructor({x, y, radius, color}) {
+    /** @type { Vector } */
+    this.pos = new Vector(x, y);
+    /** @type { Vector } */
+    this.origin = this.pos.copy();
+    /** @type { Vector } */
+    this.acc = new Vector(0, 0);
+    /** @type { Vector } */
+    this.vel = new Vector(0, 0);
+    /** @type { number } */
+    this.radius = radius;
+    /** @type { string } */
+    this.color = color;
+    this.scale = 1;
+  }
+
+  /**
+   * 
+   * @param { Vector } force 
+   */
+  applyForce(force) {
+    this.acc.add(force);
+  }
+
+  attract() {
+    const force = this.origin.copy();
+    force.sub(this.pos);
+    if (force.mag === 0) {
+      return;
+    }
+    force.mult(k / force.mag);
+    this.applyForce(force);
+  }
+
+  repel(x, y) {
+    const dx = this.pos.x - x;
+    const dy = this.pos.y - y;
+    if (dx * dx + dy * dy > repelRadius) {
+      return;
+    }
+    const force = new Vector(dx, dy);
+    force.mult(c / force.mag);
+    this.applyForce(force);
+  }
+
+  update() {
+    this.vel.add(this.acc);
+    this.vel.mult(d);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+    const dx = this.pos.x - this.origin.x;
+    const dy = this.pos.y - this.origin.y;
+    this.scale = mapRange(dx * dx + dy * dy, 0, ratioRadius, 1, 5, true);
+  }
+
+  /**
+   * 
+   * @param { CanvasRenderingContext2D } context 
+   */
+  draw(context) {
+    
+    context.beginPath();
+    context.arc(this.pos.x, this.pos.y, this.scale * this.radius, 0, 2 * Math.PI);
+    context.fillStyle = this.color;
+    context.strokeStyle = this.color;
+    context.stroke();
+    context.fill();
+  }
+}
