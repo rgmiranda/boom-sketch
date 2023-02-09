4c059c10aaab20c4eb75010d8ea10643045477e5
Ricardo Miranda <ricardo.miranda@geopagos.com>
2023-02-09 15:01:04 -0300

    More synth lines

--- synthlines.js
+++ synthlines.js
@@ -1,4 +1,6 @@
 const canvasSketch = require('canvas-sketch');
+const { offsetHSL } = require('canvas-sketch-util/color');
+const { rangeFloor } = require('canvas-sketch-util/random');
 const createColorMap = require('colormap');
 
 const cvWidth = 1080;
@@ -31,12 +33,20 @@ const pointPadding = cvHeight / (numRows + 1);
 
 const nshades = numLines;
 const colormap = 'magma'
-const colors = createColorMap({
+const skyColors = createColorMap({
   colormap,
   nshades,
   format: 'hex',
   alpha: 1
 });
+
+const numBuildings = rangeFloor(12, 20);
+const buildingsColors = createColorMap({
+  colorma: 'copper',
+  nshades,
+  format: 'hex',
+  alpha: 1
+}).map(c => offsetHSL(c, 0, 0, -25).hex);
 
 const settings = {
   dimensions: [cvWidth, cvHeight]
@@ -71,10 +81,10 @@ class Line {
 const lines = [];
 
 const sketch = () => {
-  let y, lineWidth, lineColor;
+  let x, y, lineWidth, lineColor;
   for (let i = 0; i < numLines; i ++) {
     y = (i + 1) * linePadding;
-    lineColor = colors[i];
+    lineColor = skyColors[i];
     lines.push(new Line({
       sx: pointPadding,
       sy: y,  
@@ -109,28 +119,46 @@ const sketch = () => {
       c.draw(context);
     }
     context.restore();
-/*
+
+    x = linePadding;
+    y = height;
+
     context.save();
-    angle = Math.PI * 0.5;
-    context.rotate(angle);
-    angle = -Math.PI * 0.5;
-    context.translate(translations[1].x, translations[1].y);
+
     context.beginPath();
-    context.moveTo(width * 0.5 + Math.cos(angle) * radius, height * 0.5 + Math.sin(angle) * radius);
-    for (let i = 1; i < 3; i++) {
-      angle += Math.PI * 2 / 3;
-      context.lineTo(width * 0.5 + Math.cos(angle) * radius, height * 0.5 + Math.sin(angle) * radius);
+    context.moveTo(x, y);
+
+    let isLast = false;
+    while (!isLast) {
+      const h = rangeFloor(height * 0.7, height * 0.9);
+      const w = rangeFloor(width / numBuildings - 10, width / numBuildings + 10);
+
+      y = h;
+      context.lineTo(x, y);
+      x += w;
+      if (x > width - linePadding) {
+        x = width - linePadding;
+        isLast = true;
+      }
+      context.lineTo(x, y);
     }
-    context.closePath();
+    context.lineTo(width - linePadding, height );
     context.clip();
 
     context.fillStyle = 'black';
     context.fillRect(0, 0, width, height);
 
-    for (const c of lines) {
-      c.draw(context);
+    for (let i = 0; i < numLines; i++) {
+      x = (i + 1) * linePadding;
+      y = linePadding;
+      context.beginPath();
+      context.moveTo(y, x);
+      y = height - linePadding;
+      context.lineTo(y, x);
+      context.strokeStyle = buildingsColors[i];
+      context.stroke();
     }
-    context.restore();*/
+    context.restore();
 
   };
 };
