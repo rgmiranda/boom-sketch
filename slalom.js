7a7d0c5bcb4688c0f31155d5d34c4ab8bc2d9f3b
Ricardo Miranda <ricardo.miranda@geopagos.com>
2023-09-08 18:13:57 -0300

    Slalom fix

--- slalom.js
+++ slalom.js
@@ -4,8 +4,8 @@ const settings = {
   dimensions: [ 1080, 1080 ]
 };
 
-const numLines = 16;
-const turns = 4;
+const numLines = 12;
+const turns = 8;
 
 const sketch = () => {
   return ({ context, width, height }) => {
@@ -17,34 +17,68 @@ const sketch = () => {
 
     context.strokeStyle = 'black';
 
-    for (let i = 1; i < turns; i++) {
+    for (let i = 0; i < turns + 1; i++) {
 
-      const x = lineWidth * i * (2 * numLines);
+      const x = i * trackWidth;
 
-      for(let j = 0; j < numLines; j++) {
-        const r = 2 * (j + 0.5) * lineWidth;
+      for (let j = 0; j < turns + 1; j++) {
 
-        context.beginPath();
-        context.moveTo(x + r, trackWidth);
-        context.lineTo(x + r, height - trackWidth);
-        context.stroke();
+        const y = j * trackWidth;
 
-        if ( i % 2 === 0 ) {
-          context.beginPath();
-          context.arc(x, trackWidth, r, Math.PI * 1.5, Math.PI * 2);
-          context.stroke();
-          context.beginPath();
-          context.arc(x + trackWidth, height - trackWidth, r, Math.PI * 0.5, Math.PI);
-          context.stroke();
-        } else {
-          context.beginPath();
-          context.arc(x + trackWidth, trackWidth, r, Math.PI, Math.PI * 1.5);
-          context.stroke();
-          context.beginPath();
-          context.arc(x, height - trackWidth, r, 0, Math.PI * 0.5);
-          context.stroke();
+        for(let k = 0; k < numLines; k++) {
+          const r = 2 * (k + 0.5) * lineWidth;
+
+          if ( j === 0 || j === turns) {
+            if ( i % 2 === 0 ) {
+              context.beginPath();
+              context.arc(x, trackWidth, r, Math.PI * 1.5, Math.PI * 2);
+              context.stroke();
+              context.beginPath();
+              context.arc(x + trackWidth, height - trackWidth, r, Math.PI * 0.5, Math.PI);
+              context.stroke();
+            } else {
+              context.beginPath();
+              context.arc(x + trackWidth, trackWidth, r, Math.PI, Math.PI * 1.5);
+              context.stroke();
+              context.beginPath();
+              context.arc(x, height - trackWidth, r, 0, Math.PI * 0.5);
+              context.stroke();
+            }
+            continue;
+          }
+          if ( i === 0 || i === turns) {
+            if ( j % 2 === 0 ) {
+              context.beginPath();
+              context.arc(trackWidth, y, r, Math.PI * 1.5, Math.PI * 2);
+              context.stroke();
+              context.beginPath();
+              context.arc(width - trackWidth, y + trackWidth, r, Math.PI * 0.5, Math.PI);
+              context.stroke(); 
+            } else {
+              context.beginPath();
+              context.arc(trackWidth, y + trackWidth, r, Math.PI, Math.PI * 1.5);
+              context.stroke();
+              context.beginPath();
+              context.arc(width - trackWidth, y, r, 0, Math.PI * 0.5);
+              context.stroke();
+            }
+            continue;
+          }
+
+          if ((i + j) % 2 == 0) {
+            context.beginPath();
+            context.moveTo(x + r, y);
+            context.lineTo(x + r, y + trackWidth);
+            context.stroke();
+          } else {
+            context.beginPath();
+            context.moveTo(x, y + r);
+            context.lineTo(x + trackWidth, y + r);
+            context.stroke();
+          }
         }
       }
+
       
     }
   };
